c7431e87cd2460493f9970886080ecc801ecb212
quoctrung163 <quoctrung163@yandex.com>
2020-02-28 18:32:31 +0700

    styling menu-item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,14 +4,14 @@ import './menu-item.styles.scss';
 
 // props.title -> {title}
 const MenuItem = ({ title, imageUrl, size }) => (
-	<div 
-      className={`${size} menu-item`}
-      style={{
-         backgroundImage: `url(${imageUrl})`
-      }}
-   >
+	<div className={`${size} menu-item`}>
+      <div className='background-image' 
+         style={{
+            backgroundImage: `url(${imageUrl})`
+         }} 
+      />
 		<div className="content">
-			<h1 className="title">{title}</h1>
+			<h1 className="title">{title.toUpperCase()}</h1>
 			<span className="subtitle">SHOP NOW</span>
 		</div>
 	</div>
