c5122489e4b5bb402102f0670b9921b496f2ae98
quoctrung163 <quoctrung163@yandex.com>
2020-03-01 08:26:00 +0700

    custom button component and done lesson 6

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -43,6 +44,8 @@ class SignIn extends React.Component {
                   value={this.state.email} 
                   label='email'
                   required 
+                  // neu co require thi khi nguoi dung ko nhap thong tin thi no se hien len truong
+                  // Please fill out this field.
                />
                
                <FormInput 
@@ -54,7 +57,7 @@ class SignIn extends React.Component {
                   required 
                />
 
-               <input type='submit' value='Submit Form' />
+               <CustomButton type='submit'> Sign in </CustomButton>
             </form>
          </div>
       )
