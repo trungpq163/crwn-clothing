677a3b1097791891dca67ca644425aca4e36224f
quoctrung163 <miyucoder@gmail.com>
2020-03-03 14:16:59 +0700

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -34,9 +34,10 @@ class App extends React.Component {
 	}
 
 	render() {
+		const { currentUser } = this.state;
 		return (
 			<div>
-				<Header />
+				<Header currentUser={currentUser} />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -56,12 +56,16 @@ class SignIn extends React.Component {
                   label='password'
                   required 
                />
-
-               <CustomButton type='submit'> Sign in </CustomButton>
-               <CustomButton onClick={signInWithGoogle}> 
-                  {' '}
-                  Sign In With Google{' '} 
-               </CustomButton>
+               <div className='buttons'>
+                  <CustomButton type='submit'> Sign in </CustomButton>
+                  <CustomButton 
+                     onClick={signInWithGoogle}
+                     isGoogleSignIn
+                  > 
+                     {' '}
+                     Sign In With Google{' '} 
+                  </CustomButton>
+               </div>
             </form>
          </div>
       )
