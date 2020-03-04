9c3c14c0ecb052cb3acfce2ec9adf696e2ab3a00
quoctrung163 <miyucoder@gmail.com>
2020-03-04 21:01:26 +0700

    commit and mapstate to props

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {
-	auth,
-	createUserProfileDocument
-} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 	constructor() {
@@ -17,29 +14,29 @@ class App extends React.Component {
 
 		this.state = {
 			currentUser: null
-		}
+		};
 	}
 
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {	
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 
-				userRef.onSnapshot(snapShot => {
+				userRef.onSnapshot((snapShot) => {
 					this.setState({
 						currentUser: {
 							id: snapShot.id,
 							...snapShot.data()
 						}
-					})
+					});
 				});
 			}
-			
+
 			this.setState({
 				currentUser: userAuth
-			})
+			});
 		});
 	}
 
@@ -48,10 +45,9 @@ class App extends React.Component {
 	}
 
 	render() {
-		const { currentUser } = this.state;
 		return (
 			<div>
-				<Header currentUser={currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
