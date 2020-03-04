755271d4cd1215749643292d24818e64900a5231
quoctrung163 <miyucoder@gmail.com>
2020-03-04 14:31:58 +0700

    change

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,23 +8,28 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-   <div className='header'>
-      <Link className='logo-container' to='/'>
-         <Logo className='logo' />
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
       </Link>
-
-      <div className='options'>
-         <Link className='option' to='/shop'>SHOP</Link>
-         <Link className='option' to='/contact'>CONTACT</Link>
-         {
-            currentUser ?
-            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-         }
-      </div>
-   </div>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+    </div>
+  </div>
 );
 
 export default Header;
-
