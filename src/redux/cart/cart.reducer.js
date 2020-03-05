b4b072941b5b0ceb5de258b7b109c6f106b14d10
quoctrung163 <miyucoder@gmail.com>
2020-03-05 16:09:55 +0700

    updated shop data w new data, updated cart utils to gr items in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,6 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
+
 
 const INITIAL_STATE = {
     hidden: true,
@@ -16,11 +18,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
          default:
              return state;   
     }
 }
 
-export default cartReducer
+export default cartReducer;
