cc43743ac514ecbd4289f25dcba7fe26e6643b66
quoctrung163 <miyucoder@gmail.com>
2020-03-06 09:14:15 +0700

    selection in redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -27,6 +27,6 @@ const CollectionItem = ({ item, addItem }) => {
 
 const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
-})
+});
 
 export default connect(null, mapDispatchToProps)(CollectionItem); 
